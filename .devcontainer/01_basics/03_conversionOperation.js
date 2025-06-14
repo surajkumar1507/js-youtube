2432c6dcc73f3f1461114e49f48203650e1495b4
surajkumar1507 <surajkumar.stwc@gmail.com>
2025-06-14 14:59:28 +0000

    03_conversionOperation

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score = "33abc"
+console.log(typeof (score));
+let valueInNumber = Number(score)
+console.log(typeof (valueInNumber));
+// "33" => 33
+// "33abc" => NaN but type is number
+// true => 1; false => 
+
+let isLoggedIn = "Suraj"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false 
+// ""  => false
+// "Suraj" => true
+
+let someNumber = 33
+ 
+let stringNumber = String(someNumber);
+
+console.log(typeof(someNumber));
