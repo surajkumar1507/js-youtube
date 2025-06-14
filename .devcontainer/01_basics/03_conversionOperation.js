018654da596ca8993071ebc465436983f62368b9
surajkumar1507 <surajkumar.stwc@gmail.com>
2025-06-14 15:53:54 +0000

    04_comparision

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -1,7 +1,7 @@
 let score = "33abc"
-console.log(typeof (score));
+// console.log(typeof (score));
 let valueInNumber = Number(score)
-console.log(typeof (valueInNumber));
+// console.log(typeof (valueInNumber));
 // "33" => 33
 // "33abc" => NaN but type is number
 // true => 1; false => 
@@ -9,7 +9,7 @@ console.log(typeof (valueInNumber));
 let isLoggedIn = "Suraj"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false 
 // ""  => false
@@ -19,4 +19,10 @@ let someNumber = 33
  
 let stringNumber = String(someNumber);
 
-console.log(typeof(someNumber));
+// console.log(typeof(someNumber));
+
+//**************************** Operations  *******************
+
+// console.log(2**3)  2 power 3
+console.log(2 + 3 + "1"); // first data type considor first(for operation)
+
---
+++ .devcontainer/01_basics/04_comparision.js
@@ -0,0 +1,9 @@
+console.log("2" > 1 );
+console.log(null > 0); //false 
+console.log(null >= 0); //true
+
+console.log(undefined >= 0 );
+
+// ===  check data type, value 
+
+console.log("2" === 2); //false
