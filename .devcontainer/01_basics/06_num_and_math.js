2688f44489a0c2894db356c841decd059bd37e43
surajkumar1507 <surajkumar.stwc@gmail.com>
2025-06-18 13:35:23 +0000

    05_String.js

---
+++ .devcontainer/01_basics/06_num_and_math.js
@@ -0,0 +1,25 @@
+const score = 400
+
+const balance = new Number(100) // number data types
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+
+const othernumber = 123.8966
+console.log(othernumber.toPrecision(4));
+
+const hundreds = 1000000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+//*******************Math*************************
+console.log(Math);
+console.log(Math.round(4.3))
+console.log(Math.max(1, 2, 5, 9, 6 , 7))
+console.log(Math.random()*10)
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1)) + min)
---
+++ .devcontainer/01_basics/dataTypes_summary.js
@@ -0,0 +1,14 @@
+// Primitiv => call by value (if call a fuction you got the copy value of data , if you change the value of data in another function it isn't change originally)
+// 7 Type : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+// Non-Primitive (call by Reference) => the values of data is allocated in reference
+ // Arrays, Object, Function
+
+ const heros = ["shaktiman", "doga", "najraj"];
+ console.log(typeof heros);
+
+
+ // ****************************************************
+
+// Stack (Primitive),  call by value
+// Heap(Non-Primitive)  call by reference
